b65272b7bca7bedcc02baf9d791c8eeefe32d33b
Emil Zaripov <emil.zaripov@ibm.com>
2021-08-21 13:35:49 +0300

    feat: ports

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   await app.listen(PORT);
 
